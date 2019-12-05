9ec6a4f037e9a29c0f01f2dd05e60c1a2f0f1a0e
arata6699 <arata.66@icloud.com>
2019-12-05 19:30:06 +0900

    s

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,3 @@
-server '3.114.251.64', user: 'ec2-user', roles: %w{app db web}
-
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
